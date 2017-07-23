81ddd4186d42a0a51ec9aa9f5a380a6791e09566
kulmerov <kulmerov@gmail.com>
2017-07-23 16:25:14 +0300

    update path to Gollum config file

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cp -r ~/host-ssh/ ~/.ssh/
 chmod 700 ~/.ssh && chmod 600 ~/.ssh/*
-gollum --port 80 --config /usr/local/bundle/etc/gollum-config.rb
+gollum --port 80 --config /usr/local/etc/gollum-config.rb
